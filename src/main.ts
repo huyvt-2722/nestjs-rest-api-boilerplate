fa5707b822bf07ffe1707c286e5b5d1cdc5efc0c
Vu The Huy <vuthehuy.t61@gmail.com>
2024-11-24 03:33:32 +0700

    feat: add middleware

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import compression from 'compression';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('/api');
+  app.use(helmet());
+  app.use(compression());
+  app.use(morgan('combined'));
+  app.enableVersioning();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
